bbec8b867a9b0719918e941d4c893702295ced84
asd1012808834 <asd1012808834@sina.com>
2020-12-04 00:04:30 +0800

    聊天区模块上传

---
+++ chat-client/src/main/java/com/chat/chatclient/ChatClientApplication.java
@@ -0,0 +1,15 @@
+package com.chat.chatclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ChatClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatClientApplication.class, args);
+	}
+
+}

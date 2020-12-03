e36a3b0e8bf6cfb9cc8b66abe8c60500330311f4
asd1012808834 <asd1012808834@sina.com>
2020-12-03 22:32:01 +0800

    测试democlient-已成功 原因：程序只加载Application.java所在包及其子包下的内容

--- eureka-client/src/main/java/com/eurekaclient/eurekaclient/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/eurekaclient/eurekaclient/EurekaClientApplication.java
@@ -3,13 +3,19 @@ package com.eurekaclient.eurekaclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class EurekaClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaClientApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(EurekaClientApplication.class, args);
+
+		Environment environment = context.getBean(Environment.class);
+
+		System.out.println("访问链接：http://localhost:" +environment.getProperty("server.port")+environment.getProperty("server.servlet.context-path"));
 	}
 
 }
